383d0366ab7da40c024f593d2befa211e4b67183
El Mouttaqui Yaakoub <mmoutaki05@gmail.com>
2025-05-27 09:58:24 -0400

    yaakoub
    cccccbc

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-# affichage les numero de 1 - 10
-
-for i in range(1,11):
-    print(i)
+mylist=[1,3,5,7,11]
+i,j=0,len(mylist)-1
+while(i<j):
+    temp=mylist[i]
+    mylist[i]=mylist[j]
+    i+=1
+    j-=1
+    print(mylist)
