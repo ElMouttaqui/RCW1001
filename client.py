7c12139601b8259063b166c5bb2710bb6c144596
El Mouttaqui Yaakoub <mmoutaki05@gmail.com>
2025-06-02 16:53:24 -0400

    updated

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 
-#url='http://localhost:8000'
+# url='http://localhost:8000'
 url='https://api-students-rcw-h2gchucugmayhph6.canadacentral-01.azurewebsites.net/test'
 response=requests.get(url)
 response=response.json()
