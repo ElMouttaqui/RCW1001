da77f5efbe4fd7b6fb3e3125dd8661205a0f5c3d
El Mouttaqui Yaakoub <mmoutaki05@gmail.com>
2025-06-02 16:45:46 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 
-url='http://localhost:8000'
+#url='http://localhost:8000'
+url='https://api-students-rcw-h2gchucugmayhph6.canadacentral-01.azurewebsites.net/test'
 response=requests.get(url)
 response=response.json()
 print(response['message'])
